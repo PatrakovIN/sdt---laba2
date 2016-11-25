7e065c4498857e807ff904f317fa29209fa48e7e
PatrakovIN <puzirek96>
2016-11-25 11:58:09 +0400

    11111111uuyuyuyuyuyzz

---
+++ lab_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// This program is just an example one under VCS.
+int main()
+{
+int
+a, b;
+cout << "Enter A and B: ";
+cin  >> a >> b;
+cout << "A + B = " << a + b << '\n'<< "A -B = " << a -b << '\n'<< "A * B = " << a * b << '\n';
+}
+
+
---
+++ mai.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "input.h"
+// This program is just an example one under VCS.
+
+int
+main()
+{
+int
+a, b;
+cout << "Enter A and B: ";
+cin  >> a >> b;
+cout << "A + B = " << a + b << '\n'<< "A -B = " << a -b << '\n'<< "A * B = " << a * b << '\n';
+}
